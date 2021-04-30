11e85891db630d50752706bd72bd6a71c9c12440
Khalin7494 <khalin@protonmail.com>
2021-04-30 21:04:07 +0300

    Hello 2

--- src/first/Lecture_1.java
+++ src/first/Lecture_1.java
@@ -3,5 +3,6 @@ package first;
 public class Lecture_1 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("ИИИИИИИИИИИИИИИИИИИИИ?");
     }
 }

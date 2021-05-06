fb4ce44b7261fa1a78f47f85dee9cbc2e4492187
Khalin7494 <khalin@protonmail.com>
2021-05-06 21:52:02 +0300

    HW1 Task2

---
+++ src/uaKhalin/HW1_Task2.java
@@ -0,0 +1,13 @@
+package uaKhalin;
+
+import java.util.Date;
+
+public class HW1_Task2 {
+    public static void main(String[] args) {
+        String name = "Евгений ";
+        String surname = "Халин, ";
+
+        System.out.println(name + surname + new Date());
+    }
+}
+

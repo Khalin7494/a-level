ac99a5f9e05c32dc15d13a7a505ad8b33b8c61d0
Khalin7494 <khalin@protonmail.com>
2021-05-07 11:28:55 +0300

    HW2 Task2

--- src/uaKhalin/HW2_Task2.java
+++ src/uaKhalin/HW2_Task2.java
@@ -1,8 +1,8 @@
-package SecondLecture;
+package uaKhalin;
 
 import java.util.Scanner;
 
-public class Lecture2_Task2 {
+public class HW2_Task2 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 

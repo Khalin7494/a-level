8e6b333092adb2537bd6149207b5f0ceaa46c00d
Khalin7494 <khalin@protonmail.com>
2021-05-08 15:58:58 +0300

    HW3 Task3

--- src/uaKhalin/HW3/HW3_Task3.java
+++ src/uaKhalin/HW3/HW3_Task3.java
@@ -5,15 +5,13 @@ import java.util.Scanner;
 public class HW3_Task3 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
+        System.out.println("Введите строку: ");
         System.out.println("Количество слов в строке: " + countWords(console.nextLine()));
-
     }
 
     public static int countWords(String text) {
-        String[] words = text.split("\s+");
-
-        int count = words.length;
-        return count;
+        String[] words = text.split("[^A-Za-zА-Яа-я]+");
+        return words.length;
 
     }
 }

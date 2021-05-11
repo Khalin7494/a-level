f4a6c2194a5e6542336c08f026db65b4f7aa515c
Khalin7494 <khalin@protonmail.com>
2021-05-11 18:26:16 +0300

    HW4 Task1

---
+++ src/uaKhalin/HW4/HW4_Task1.java
@@ -0,0 +1,20 @@
+package uaKhalin.HW4;
+
+public class HW4_Task1 {
+    public static void main(String[] args) {
+        int[] randomArray = new int[400];
+        for (int i = 0; i < randomArray.length; i++) {
+            randomArray[i] = (int) (Math.random() * 10) + 1;
+        }
+        int count = 0;
+        double multiplication = 1.0;
+        for (int i = 0; i < randomArray.length; i++) {
+            count += randomArray[i];
+            multiplication *= randomArray[i];
+        }
+        int average = count / randomArray.length;
+        double geometric = Math.pow(multiplication, 1.0 / randomArray.length);
+        System.out.println("Среднее арифметическое число массива = " + average);
+        System.out.println("Среднее геометрическое число массива = " + geometric);
+    }
+}

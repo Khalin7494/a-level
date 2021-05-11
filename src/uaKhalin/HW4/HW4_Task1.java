2c73d97d03af7106e597c14e48f4e2db4c5fce15
Khalin7494 <khalin@protonmail.com>
2021-05-11 19:09:36 +0300

    HW4 Task1

--- src/uaKhalin/HW4/HW4_Task1.java
+++ src/uaKhalin/HW4/HW4_Task1.java
@@ -6,11 +6,11 @@ public class HW4_Task1 {
         for (int i = 0; i < randomArray.length; i++) {
             randomArray[i] = (int) (Math.random() * 10) + 1;
         }
-        double count = 0;
+        double count = 0.0;
         double multiplication = 1.0;
-        for (int j : randomArray) {
-            count += j;
-            multiplication *= j;
+        for (int i = 0; i < randomArray.length; i++) {
+            count += randomArray[i];
+            multiplication *= randomArray[i];
         }
         double average = count / randomArray.length;
         double geometric = Math.pow(multiplication, 1.0 / randomArray.length);

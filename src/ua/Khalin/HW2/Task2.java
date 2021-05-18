bc33f5952d799186973c70048898a89b71dedd13
Khalin7494 <khalin@protonmail.com>
2021-05-18 14:50:54 +0300

    HW2 Task2

---
+++ src/ua/Khalin/HW2/Task2.java
@@ -0,0 +1,20 @@
+package ua.Khalin.HW2;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        triangle(console.nextInt(), console.nextInt(), console.nextInt(), console.nextInt(), console.nextInt(), console.nextInt());
+    }
+
+    public static void triangle(int x1, int x2, int x3, int y1, int y2, int y3) {
+        int s = ((x1 - x3) * (y2 - y3) - (y1 - y3) * (x2 - x3)) / 2;
+        if (s < 0) {
+            s = Math.abs(s);
+        }
+
+        System.out.println(s);
+    }
+}
+

a927a58d41f97f46fdd62ad1c56130c5ec109842
Khalin7494 <khalin@protonmail.com>
2021-05-18 14:54:57 +0300

    HW2 Task4

---
+++ src/ua/Khalin/HW2/Task4.java
@@ -0,0 +1,23 @@
+package ua.Khalin.HW2;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        double firstNumber = console.nextDouble();
+        double secondNumber = console.nextDouble();
+        double thirdNumber = console.nextDouble();
+        minimum(firstNumber, secondNumber, thirdNumber);
+    }
+
+    public static void minimum(double a, double b, double c) {
+        a = a < 0.0D ? -a : a;
+        b = b < 0.0D ? -b : b;
+        c = c < 0.0D ? -c : c;
+        double min = a < b ? a : b;
+        min = c < min ? c : min;
+        System.out.println(min);
+    }
+}
+

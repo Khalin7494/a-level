81933aa685ac632619d7962462d4f133740bf39f
Khalin7494 <khalin@protonmail.com>
2021-05-18 15:15:12 +0300

    HW4 Task4

---
+++ src/ua/Khalin/HW4/Task4.java
@@ -0,0 +1,18 @@
+package ua.Khalin.HW4;
+
+import java.util.Arrays;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int[] randomArray = new int[2000];
+        for (int i = 0; i < randomArray.length; i++) {
+            randomArray[i] = (int) (Math.random() * 10) + 1;
+        }
+        System.out.println(Arrays.toString(randomArray));
+        for (int i = 0; i < randomArray.length; i++) {
+            if (randomArray[i] % 2 == 0)
+                randomArray[i] = 0;
+        }
+        System.out.println(Arrays.toString(randomArray));
+    }
+}

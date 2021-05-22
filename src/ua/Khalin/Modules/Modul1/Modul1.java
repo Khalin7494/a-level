706535cbf3a26eb712ab54e9895656390a07cdcb
Khalin7494 <khalin@protonmail.com>
2021-05-23 00:42:44 +0300

    Modul 1 Task1

--- src/ua/Khalin/Modules/Modul1/Modul1.java
+++ src/ua/Khalin/Modules/Modul1/Modul1.java
@@ -2,12 +2,23 @@ package ua.Khalin.Modules.Modul1;
 
 public class Modul1 {
     public static void main(String[] args) {
-        int[] array = new int[]{4, 45, 44, 13, 1, 25};
+        int[] array = new int[]{45, 4, 77, 89, 89};
 
-        System.out.println("Количество элементов массива - " + lengthOfArray(array));
+        System.out.println("Количество уникальных элементов массива - " + countNumbersUniq(array));
     }
 
-    public static int lengthOfArray(int[] array) {
-        return array.length;
+    public static int countNumbersUniq(int[] array) {
+        int count = 0;
+        int temp = 1;
+        for (int i = 0; i < array.length - 1; i++) {
+            temp++;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] == array[i]) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return temp - count;
     }
 }

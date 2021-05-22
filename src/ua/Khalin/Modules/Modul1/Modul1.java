08a330f1d08ccd6113f71e3ade347a62ab976bac
Khalin7494 <khalin@protonmail.com>
2021-05-23 00:50:58 +0300

    Modul 1 Task1

--- src/ua/Khalin/Modules/Modul1/Modul1.java
+++ src/ua/Khalin/Modules/Modul1/Modul1.java
@@ -1,10 +1,16 @@
 package ua.Khalin.Modules.Modul1;
 
+import java.util.Arrays;
+
 public class Modul1 {
     public static void main(String[] args) {
-        int[] array = new int[]{45, 4, 77, 89, 89};
+        int[] randomArray = new int[10];
+        for (int i = 0; i < randomArray.length; i++) {
+            randomArray[i] = (int) (Math.random() * 10) + 1;
+        }
+        System.out.println("Изначальный рандомный массив - " + Arrays.toString(randomArray));
 
-        System.out.println("Количество уникальных элементов массива - " + countNumbersUniq(array));
+        System.out.println("Количество уникальных элементов массива - " + countNumbersUniq(randomArray));
     }
 
     public static int countNumbersUniq(int[] array) {

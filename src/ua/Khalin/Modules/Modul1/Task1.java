32fec50dcbe0679bfbbfcdc2fd38506bcb7262a3
Khalin7494 <khalin@protonmail.com>
2021-05-23 00:55:23 +0300

    Modul 1 Task1

--- src/ua/Khalin/Modules/Modul1/Task1.java
+++ src/ua/Khalin/Modules/Modul1/Task1.java
@@ -2,7 +2,7 @@ package ua.Khalin.Modules.Modul1;
 
 import java.util.Arrays;
 
-public class Modul1 {
+public class Task1 {
     public static void main(String[] args) {
         int[] randomArray = new int[10];
         for (int i = 0; i < randomArray.length; i++) {
